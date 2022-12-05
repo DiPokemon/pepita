7432b3a83644649fb847d7016e7c875f6ce3252e
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2022-12-05 20:54:59 +0300

    фикс скрипта равной высоты

--- static/js/inits.js
+++ static/js/inits.js
@@ -195,5 +195,19 @@ $(document).ready(function () {
     // }); 
 
     
- 
+    function setEqualHeight(columns){
+        var tallestcolumn = 0;
+        columns.each(
+            function(){
+                currentHeight = $(this).height();
+                if(currentHeight > tallestcolumn){
+                    tallestcolumn = currentHeight;
+                }
+            }
+        );
+        columns.attr('style','height:' + tallestcolumn + 'px');
+    }
+    
+        setEqualHeight($(".portfolio_item img"));
+   
 });
